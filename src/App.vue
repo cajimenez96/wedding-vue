3dca64d938ab35279542ddbf2e4c97677f6eec84
Carlos Jimenez <carlos.exejimenez@gmail.com>
2025-08-09 11:22:32 -0300

    Feat: initial project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  function handleClick (nombre: string) {
+    console.log("Bienvenido " + nombre);
+  }
+
+  const handleClickDos = () => {
+    console.log("Funcion flecha!");
+  }
+
+</script>
+
+<template class="container">
+
+  <button v-on:click="handleClick('Lucas')">Funcion convencional</button>
+  <br>
+  <br>
+  <button v-on:click="handleClickDos()">Funcion Flecha</button>
+</template>
+
+<style scoped>
+.container {
+  border: 1px solid red;
+}
+</style>
